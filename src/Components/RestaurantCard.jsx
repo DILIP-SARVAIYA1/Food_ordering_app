8f35d386e495af3779e0c9083532e64e63a7e917
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2024-03-17 21:17:41 +0530

    New Shimmer added

--- src/Components/RestaurantCard.jsx
+++ src/Components/RestaurantCard.jsx
@@ -6,16 +6,23 @@ const RestaurantCard = (props) => {
   const { userLogIn } = useContext(UserContext);
   const { name, cloudinaryImageId, cuisines, sla } = props.resData.info;
   return (
-    <div className="w-[250px] h-full p-5 shadow-lg bg-gray-200 cursor-pointer rounded-md hover:bg-gray-300 hover:scale-95 transition-all">
+    <div
+      className="w-[250px] h-full  shadow-lg bg-gray-200 cursor-pointer rounded-md hover:bg-gray-300 hover:scale-95 transition-all"
+      onClick={() => {
+        window.scrollTo({ top: 0, left: 0 });
+      }}
+    >
       <img
-        className="w-full h-[180px] object-cover rounded-md"
+        className="w-full h-[180px] object-cover rounded-t-lg"
         src={RES_CARD_IMG + cloudinaryImageId}
         alt="Restaurants image"
       />
-      <p className="mt-2 font-bold">{name}</p>
-      <p className="text-gray-600">{cuisines.join(", ")}</p>
-      <p className="text-gray-700">{sla.deliveryTime} Minutes</p>
-      <p className="text-sm text-gray-500">User Log in: {userLogIn}</p>
+      <div className="px-5 py-2">
+        <p className="mt-2 font-bold">{name}</p>
+        <p className="text-gray-600">{cuisines.join(", ")}</p>
+        <p className="text-gray-700">{sla.deliveryTime} Minutes</p>
+        <p className="text-sm text-gray-500">User Log in: {userLogIn}</p>
+      </div>
     </div>
   );
 };
--- src/Components/ShimmerUi.jsx
+++ src/Components/ShimmerUi.jsx
@@ -1,26 +1,150 @@
 const ShimmerUi = () => {
   return (
     <div className="flex flex-wrap gap-5 justify-between my-10 px-20">
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
-      <div className="w-[250px] h-[323px] shadow-lg rounded-lg"></div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="shimmerBG media"></div>
+        <div className="p-32">
+          <div className="shimmerBG title-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+          <div className="shimmerBG content-line"></div>
+        </div>
+      </div>
     </div>
   );
 };
